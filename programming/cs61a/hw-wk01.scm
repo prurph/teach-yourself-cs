b0f236c44a4e53415e11b239d672f52309355760
Prescott Murphy <prurph@gmail.com>
2021-03-10 15:48:24 +0000

    Convert comments to double semi-colon per 'Lisp and Scheme tradition'

--- programming/cs61a/hw-wk01.scm
+++ programming/cs61a/hw-wk01.scm
@@ -1,12 +1,12 @@
-; [Questions](https://people.eecs.berkeley.edu/~bh/61a-pages/Volume1/hw.pdf)
-; [Solutions](https://people.eecs.berkeley.edu/~bh/61a-pages/Solutions/week1)
+;; [Questions](https://people.eecs.berkeley.edu/~bh/61a-pages/Volume1/hw.pdf)
+;; [Solutions](https://people.eecs.berkeley.edu/~bh/61a-pages/Solutions/week1)
 (require (planet "dyoo/simply-scheme"))
 
-; 1. SICP exercise 1.6 (see SICP solutions)
+;; 1. SICP exercise 1.6 (see SICP solutions)
 
-; 2. `squares` procedure, using procedures from dyoo/simply-scheme
-;   se: create a list from arguments ("sentence")
-;   bf: all elements of a list _b_ut the _f_irst
+;; 2. `squares` procedure, using procedures from dyoo/simply-scheme
+;;   se: create a list from arguments ("sentence")
+;;   bf: all elements of a list _b_ut the _f_irst
 (define (squares xs)
   (define (square x)
     (* x x))
@@ -38,8 +38,8 @@
       (se (switch-later-word (first sent))
           (switch-rest (bf sent)))))
 
-; 4.
-; Recursive helper
+;; 4.
+;; Recursive helper
 (define (ordered? xs)
   (define (ordered-rec? x xs)
     (if (empty? xs)
@@ -50,7 +50,7 @@
       #t
       (ordered-rec? (first xs) (bf xs))))
 
-; Recursive helper without if
+;; Recursive helper without if
 (define (ordered? xs)
   (define (ordered-rec? x xs)
     (or (empty? xs)
@@ -59,23 +59,23 @@
   (or (empty? xs)
       (ordered-rec? (first xs) (bf xs))))
 
-; Based on provided solution, but accounting for empty list. I find this the
-; easiest to parse, particularly checking the disordered case and returning
-; false instead of "else (x[0] <= x[1] && (ordered? x[1:]))", which is harder
-; to read when translated to Scheme.
+;; Based on provided solution, but accounting for empty list. I find this the
+;; easiest to parse, particularly checking the disordered case and returning
+;; false instead of "else (x[0] <= x[1] && (ordered? x[1:]))", which is harder
+;; to read when translated to Scheme.
 (define (ordered? xs)
   (cond ((empty? xs) #t)
         ((empty? (bf xs)) #t)
         ((> (first xs) (first (bf xs))) #f)
         (else (ordered? (bf xs)))))
 
-; Or, if we assume xs is non-empty
+;; Or, if we assume xs is non-empty
 (define (ordered? xs)
   (or (empty? (bf xs))
       (and (<= (first xs) (first (bf xs)))
            (ordered? (bf xs)))))
 
-; 5.
+;; 5.
 (define (word-ends-e? w)
   (equal? (last w) 'e))
 
@@ -84,7 +84,7 @@
         ((word-ends-e? (first sent)) (se (first sent) (ends-e (bf sent))))
         (else (ends-e (bf sent)))))
 
-; As a generic list filter function with a predicate
+;; As a generic list filter function with a predicate
 (define (filter l p)
   (cond ((empty? l) '())
         ((p (first l)) (se (first l) (filter (bf l) p)))
@@ -92,3 +92,4 @@
 
 (define (ends-e sent)
   (filter sent word-ends-e?))
+
