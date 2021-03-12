7a49a14a689c32d9ea23afbed2a4ca41eb75a9c0
Prescott Murphy <prurph@gmail.com>
2021-03-12 17:29:34 +0000

    Scaffold week 2 homework file

---
+++ programming/cs61a/hw-wk02.scm
@@ -0,0 +1,3 @@
+;; [Questions](https://people.eecs.berkeley.edu/~bh/61a-pages/Volume1/hw.pdf)
+;; [Solutions](https://people.eecs.berkeley.edu/~bh/61a-pages/Solutions/week2)
+(require (planet "dyoo/simply-scheme"))
