329a97b3c93565e34a4b617b58dde96c78f0f263
Prescott Murphy <prurph@gmail.com>
2021-03-13 08:43:43 +0000

    Complete Exercise 1.32

---
+++ programming/sicp/ch1/ex-1.32.scm
@@ -0,0 +1,25 @@
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-z-h-12.html#%_thm_1.32
+
+;; 1.32(a)
+;; Provide a monoid with:
+;;   - combine -> combiner
+;;   - empty -> null-value
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+;; 1.32(b)
+;; accumulate above is recursive, iterative solution:
+(define (acccumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        null-value
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
