d2880ce00b1c095ed0ffeb2ef6a92c2f6cb73910
Prescott Murphy <prurph@gmail.com>
2021-03-14 09:29:31 +0000

    Complete Week 2 homework problem 2

--- programming/cs61a/hw-wk02.scm
+++ programming/cs61a/hw-wk02.scm
@@ -1,3 +1,13 @@
 ;; [Questions](https://people.eecs.berkeley.edu/~bh/61a-pages/Volume1/hw.pdf)
 ;; [Solutions](https://people.eecs.berkeley.edu/~bh/61a-pages/Solutions/week2)
 (require (planet "dyoo/simply-scheme"))
+
+;; 1. SICP 1.31(a), 1.32(a), 1.33, 1.40, 1.41, 1.43, 1.46 (see SICP solutions)
+
+;; 2.
+;; Unconventional naming: they suggest you write map and call it every
+(define (every f xs)
+  (if (empty? xs)
+      '()
+      (se (f (first xs)) (every f (bf xs)))))
+
