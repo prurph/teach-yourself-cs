eea264af72d5794ea2b5b1fb0f4a3e7871e31517
Prescott Murphy <prurph@gmail.com>
2021-03-27 14:22:10 -0400

    Complete Exercise 2.12

---
+++ programming/sicp/ch2/ex-2.12.scm
@@ -0,0 +1,15 @@
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-14.html#%_thm_2.12
+
+;; Provided
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (let (( delta (* c p 0.01)))
+    (make-interval (- c delta) (+ c delta))))
+(define (tolerance i)
+  (* 100 (/ (width i) (center i))))
