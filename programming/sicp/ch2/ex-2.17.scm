c206bea866e0bfb3c70c38b5a51de7cce4fff0f2
Prescott Murphy <prurph@gmail.com>
2021-03-30 14:59:09 -0400

    Complete Exercise 2.17

---
+++ programming/sicp/ch2/ex-2.17.scm
@@ -0,0 +1,10 @@
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-15.html#%_thm_2.17
+
+(define (last-pair l)
+  (if (null? (cdr l))
+      l
+      (last-pair (cdr l))))
+
+;; Using existing procedures
+(define (last-pair l)
+  (list-ref l (- (length l) 1)))
