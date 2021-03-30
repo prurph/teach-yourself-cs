b121b3ddfa251fc3515d61806d5b389d2697bbd8
Prescott Murphy <prurph@gmail.com>
2021-03-30 15:37:35 -0400

    Complete Exercise 2.19

---
+++ programming/sicp/ch2/ex-2.19.scm
@@ -0,0 +1,22 @@
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-15.html#%_thm_2.19
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount (except-first-denomination coin-values))
+             (cc (- amount (first-denomination coin-values)) coin-values)))))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+;; The order of the coins does not matter. The procedure exhaustively traverses
+;; all remaining coins in the list--there is no short-circuiting based on the
+;; amount of the first-denomination, for example, only when there are no more
+;; coins remaining, or the amount is zero.
