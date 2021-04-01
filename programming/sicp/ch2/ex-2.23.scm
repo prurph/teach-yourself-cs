843a6f23f44dfa3e29c13c35b6ff0953c3a4209e
Prescott Murphy <prurph@gmail.com>
2021-04-01 16:19:29 -0400

    Complete Exercise 2.23

---
+++ programming/sicp/ch2/ex-2.23.scm
@@ -0,0 +1,14 @@
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-15.html#%_thm_2.23
+
+;; One option: use begin to run multiple expressions in else block
+(define (for-each f l)
+  (if (null? l)
+      (void)
+      (begin (f (car l))
+             (for-each f (cdr l)))))
+
+;; Another option: use `cond`, which can contain more than two expressions
+(define (for-each f l)
+  (cond ((null? l) (void))
+        (else (f (car l))
+              (for-each f (cdr l)))))
