9b7b213c34bbb43870dc841c3452db241e76c28b
Prescott Murphy <prurph@gmail.com>
2021-04-17 15:32:52 -0400

    Complete Exercise 2.36

---
+++ programming/sicp/ch2/ex-2.36.scm
@@ -0,0 +1,21 @@
+#lang scheme
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-15.html#%_thm_2.36
+
+(load "accumulate.scm")
+
+;; Given a list of lists all of identical length, accumulate the matching
+;; indexes of each sublist, and return a list of these accumulations.
+(define (accumulate-n op init seqs)
+  ;; Recursing on a lists of lists, so we're done when the first--and therefore
+  ;; all because they are the same length--is empty, e.g. ('() '() ...)
+  (if (null? (car seqs))
+      '()
+      ;; Accumulate the first element of each list, then recurse with
+      ;; accumulate-n on the remaining portion of each sublists. My initial pass
+      ;; had (map (lambda (x) (car x) seqs) but of course this is just creating
+      ;; a trivial lambda: just pass `car`!
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 (list '(1 2 3) '(4 5 6) '(7 8 9) '(10 11 12)))
+'(22 26 30)
