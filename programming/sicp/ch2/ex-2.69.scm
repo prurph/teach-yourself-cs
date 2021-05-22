3d17d55c9e26f12cbb58db82acbdb2d2e37f176a
Prescott Murphy <prurph@gmail.com>
2021-05-22 01:44:35 -0400

    Complete Exercise 2.69

---
+++ programming/sicp/ch2/ex-2.69.scm
@@ -0,0 +1,21 @@
+#lang scheme
+
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-16.html#%_thm_2.69
+
+(require "huffman.scm")
+
+;; Write successive-merge for an ordered set of leaves for use in the provided
+;; `generate-huffman-tree` below.
+(define (successive-merge leaf-set)
+  (let ((l (car leaf-set))
+        (r (cdr leaf-set)))
+    (if (null? r)
+        l
+        (successive-merge (cons (make-code-tree (car leaf-set)
+                                                (cadr leaf-set))
+                                (cddr leaf-set))))))
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(provide generate-huffman-tree)
