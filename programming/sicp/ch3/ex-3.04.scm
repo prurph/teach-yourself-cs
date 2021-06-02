e9195b29666de0528192c3d3fe1bff6b87f22cd8
Prescott Murphy <prurph@gmail.com>
2021-06-02 14:27:09 -0400

    Complete Exercise 3.04

---
+++ programming/sicp/ch3/ex-3.04.scm
@@ -0,0 +1,42 @@
+#lang scheme
+
+;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-20.html#%_thm_3.04
+
+(define (make-account balance password)
+  (define incorrect-attempts 0)
+  (define max-attempts 7)
+  (define (call_the_cops)
+    (print "Calling the cops! 🚨👮🚓"))
+  (define (verify-password-and pw f) 
+    (lambda (arg)
+      (if (eq? pw password)
+          (begin (set! incorrect-attempts 0) 
+                 (f arg))
+          (begin (set! incorrect-attempts (+ incorrect-attempts 1))
+                 (if (> incorrect-attempts max-attempts)
+                     (call_the_cops)
+                     #f)
+                 (error "Invalid password")))))
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        (error "Insufficient funds")))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  ;; Could also verify password first for any request, however doing it this
+  ;; way decouples verification from dispatch so that we could have methods that
+  ;; don't require verification.
+  (define (dispatch pw m)
+    (cond ((eq? m 'withdraw) (verify-password-and pw withdraw))
+          ((eq? m 'deposit) (verify-password-and pw deposit))
+          (else (error "Unknown request: MAKE-ACCOUNT" m))))
+  dispatch)
+
+(define acc (make-account 100 'WATERMELON))
+((acc 'WATERMELON 'withdraw) 10)
+((acc 'CHERRY 'withdraw) 10)
+((acc 'WATERMELON 'withdraw) 100)
+((acc 'CHERRY 'deposit) 1000000000)
+((acc 'WATERMELON 'deposit) 1000000000)
