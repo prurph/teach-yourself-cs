191eba6a9fd3c50c6fbad43653eaead35378de88
Prescott Murphy <prurph@gmail.com>
2021-06-20 16:20:44 -0400

    Implement make-wire

--- programming/sicp/ch3/circuit.scm
+++ programming/sicp/ch3/circuit.scm
@@ -1,6 +1,28 @@
 #lang racket
 
-(define (make-wire) (error "Not implemented"))
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin ((car procedures))
+             (call-each (cdr procedures)))))
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation: WIRE" m))))
+    dispatch))
+
 (define (get-signal wire) (error "Not implemented"))
 (define (set-signal! wire value) (error "Not implemented"))
 ;; proc is a procedure of no arguments that should be run whenever the signal
