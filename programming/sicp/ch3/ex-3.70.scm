1339d70b60eb3468a3f60d4ce6663079da143661
Prescott Murphy <prurph@gmail.com>
2021-07-18 12:21:16 -0400

    Rewrite merge-weighted to retain duplicates, and adjust weighted-pairs to compensate

--- programming/sicp/ch3/ex-3.70.scm
+++ programming/sicp/ch3/ex-3.70.scm
@@ -4,6 +4,9 @@
 
 ;; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-24.html#%_thm_3.70
 
+;; Note this does not remove duplicates, unlike merge. This is crucial for its
+;; use in finding Ramanujan numbers in ex-3.71, where we require distinct
+;; elements with identical weights to all appear.
 (define (merge-weighted s1 s2 weight)
   (cond ((stream-null? s1) s2)
         ((stream-null? s2) s1)
@@ -21,14 +24,26 @@
                                                 (stream-cdr s2)
                                                 weight)))
                   (else
-                    (cons-stream s1car
-                                 (merge-weighted (stream-cdr s1)
-                                                 (stream-cdr s2)
-                                                 weight))))))))
+                   (cons-stream s1car
+                                (cons-stream s2car
+                                (merge-weighted (stream-cdr s1)
+                                                (stream-cdr s2)
+                                                weight)))))))))
 
-(define (weighted-pairs s t weight)
+;; This has duplicates based on the implementation of merge-weighted above
+(define (weighted-pairs-dupes s t weight)
   (let ((p (pairs s t)))
     (merge-weighted p p weight)))
+
+;; This does not, but requires (stream-car s) (stream-car t) be the first pair
+(define (weighted-pairs s t weight)
+  (cons-stream (list (stream-car s) (stream-car t))
+               (merge-weighted (stream-map (lambda (x) (list (stream-car s) x))
+                                           (stream-cdr t))
+                               (weighted-pairs (stream-cdr s)
+                                               (stream-cdr t)
+                                               weight)
+                               weight)))
 
 ;; 3.70(a) Stream of all pairs of positive integers i <= j ordered according to
 ;; i + j.
@@ -40,14 +55,15 @@
 
 ;; (1 1)
 ;; (1 2)
+;; (1 3)
 ;; (2 2)
 ;; (1 3)
+;; (1 4)
 ;; (2 3)
 ;; (1 4)
-;; (3 3)
 ;; (1 5)
 ;; (2 4)
-;; (1 6)
+
 
 ;; 3.70(b) Stream of all pairs of positive integers i <= j where neither i nor
 ;; j is divisible by 2, 3 or 5, and the pairs are ordered according to
@@ -76,3 +92,6 @@
 ;; (1 17)
 ;; (7 13)
 ;; (1 19)
+
+(#%provide merge-weighted)
+(#%provide weighted-pairs)
